896f8cda0c8d8138983ef6d134d204b45e3bd307
Juan Jacobo Delgado Melo <150480579+jacoboDM@users.noreply.github.com>
2024-10-05 19:48:41 -0500

    Update productosModel.js

--- Compose/microservicios/productos/src/models/productosModel.js
+++ Compose/microservicios/productos/src/models/productosModel.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 
 const connection = mysql.createPool({
-    host: 'localhost',
+    host: 'db',
     user: 'root',
-    password: '',
+    password: 'root',
     port:'3306',
     database: 'productosDB'
 });
