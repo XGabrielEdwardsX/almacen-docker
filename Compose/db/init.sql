9eaa0a81b06cef68576dd90c42f84633f493094f
Juan Jacobo Delgado Melo <150480579+jacoboDM@users.noreply.github.com>
2024-10-05 19:48:02 -0500

    Update init.sql

--- Compose/db/init.sql
+++ Compose/db/init.sql
@@ -31,4 +31,5 @@ CREATE TABLE IF NOT EXISTS productos (
     precio FLOAT,
     inventario INT
 );
-INSERT INTO usuarios (nombre, email, usuario, password) VALUES ('Admin', 'admin@admin.com', 'admin', '123');
+USE usuariosDB;
+INSERT INTO usuarios (nombre, email, usuario, password) VALUES ('Admin', 'admin@admin.com', 'admin', 'admin');
